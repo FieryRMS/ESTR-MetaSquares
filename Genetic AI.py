32c57de1396bd71bf8141df41fae3e1963591511
FieryRMS <raiyanmohdshahrms@gmail.com>
2023-11-16 04:06:03 +0800

    glogging errors

--- Genetic AI.py
+++ Genetic AI.py
@@ -169,11 +169,11 @@ class MetaSquaresBoard:
     def make_move(self, p: Point):
         if not p.is_valid():
             logging.warning("Invalid move: Out of bounds")
-            gLogger.log_text("Invalid move: Out of bounds", severity="WARNING")  # type: ignore
+            gLogger.log_text("Invalid move Out of bounds", severity="WARNING")  # type: ignore
             return 0
         if not self.checkBoard(p, Player.EMPTY):
             logging.warning("Invalid move: Already occupied")
-            gLogger.log_text("Invalid move: Already occupied", severity="WARNING")  # type: ignore
+            gLogger.log_text("Invalid move Already occupied", severity="WARNING")  # type: ignore
             return 0
 
         self.board[p.x][p.y] = self.current_player
@@ -306,7 +306,7 @@ class AI_Agent:
     def get_move(self, board: MetaSquaresBoard):
         if self.lib == None:
             logging.error("ERROR: AI not initialized")
-            gLogger.log_text("ERROR: AI not initialized", severity="ERROR")  # type: ignore
+            gLogger.log_text("ERROR AI not initialized", severity="ERROR")  # type: ignore
             exit(1)
         c_board = (ctypes.c_int * len(board.linear_board))(*board.linear_board)
         x = self.lib.ai_player(self.player.value, c_board)
@@ -387,7 +387,7 @@ class MetaSquares:
                 logging.error("DATA:")
                 logging.error("AI1: " + str(self.AI1))
                 logging.error("AI2: " + str(self.AI2))
-                gLogger.log_text("AI made an invalid move: {}".format(p), severity="ERROR")  # type: ignore
+                gLogger.log_text("AI made an invalid move {}".format(p), severity="ERROR")  # type: ignore
                 exit(1)
 
         if self.gameState == State.INCOMPLETE:
@@ -458,7 +458,7 @@ if __name__ == "__main__":
             logging.info(("#" * config.HEADER_SIZE))
             logging.info("Generation: {}".format(generation).center(config.HEADER_SIZE))
             logging.info(("#" * config.HEADER_SIZE))
-            gLogger.log_text("Generation: {} started".format(generation))  # type: ignore
+            gLogger.log_text("GENERATION {} STARTED".format(generation))  # type: ignore
 
             win_loss_table = [
                 [State.DRAW for _ in range(sample_size)] for __ in range(sample_size)
@@ -492,7 +492,7 @@ if __name__ == "__main__":
                             generation, games_played, total_games, i, j
                         )
                     )
-                    gLogger.log_text("Playing game {}/{}: {} vs {}".format(games_played, total_games, i, j))  # type: ignore
+                    gLogger.log_text("Playing game {}/{} - {} vs {}".format(games_played, total_games, i, j))  # type: ignore
                     game = MetaSquares(agents[i], agents[j])
                     game.game_loop()
                     score[i] += game.getScore(agents[i].player)
@@ -556,7 +556,7 @@ if __name__ == "__main__":
                 )
             )
             logging.info(("#" * config.HEADER_SIZE) + "\n\n\n")
-            gLogger.log_text("Generation: {} completed".format(generation))  # type: ignore
+            gLogger.log_text("GENERATION {} COMPLETE".format(generation))  # type: ignore
         except KeyboardInterrupt:
             logging.info("Keyboard Interrupt")
             break
