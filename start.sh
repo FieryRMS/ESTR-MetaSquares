28e8183cf4619bb80c4c77129b12b1ddf0951ba4
FieryRMS <raiyanmohdshahrms@gmail.com>
2023-11-17 15:57:43 +0800

    added recompile before running training script to startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/bash
+mkdir -p .exes
+gcc -Wall -fexceptions -Wshadow -Wextra -O3 basic.c ai_player_1155205640.c -o .exes/basic.exe -lm
+gcc -shared -o .exes/ai_player1.dll -fPIC -O3 ai_player_1155205640.c -lm
+cp -fr .exes/ai_player1.dll .exes/ai_player2.dll
 pkill -f Genetic\ AI.py
 rm nohup.out
 nohup python3 Genetic\ AI.py & tail -f nohup.out
