2f7f593314fc07213b8dccf781c39585c4f31d05
FieryRMS <raiyanmohdshahrms@gmail.com>
2023-11-26 15:14:57 +0800

    added a small delay before tail so it is more consistent

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ gcc -Wall -fexceptions -Wshadow -Wextra -O3 basic.c ai_player_1155205640.c -o .e
 gcc -shared -o .exes/ai_player.dll -fPIC -O3 ai_player_1155205640.c -lm
 pkill -f Genetic\ AI.py
 rm nohup.out
-nohup python3 Genetic\ AI.py & tail -f nohup.out
+nohup python3 Genetic\ AI.py & sleep 0.5
+tail -f nohup.out
